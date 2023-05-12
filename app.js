1bfa69853c44f55b670af00e971972dc28002e65
Jiui <jiui0103@naver.com>
2023-05-12 18:19:53 +0900

    Connected to mongo DB

--- app.js
+++ app.js
@@ -3,6 +3,16 @@ const express = require("express")
 const app = express()
 app.use(express.urlencoded({extended: true}))
 
+const MongoClient = require('mongodb').MongoClient;
+MongoClient.connect('mongodb+srv://jiui4691:5G6jmgAHJtsJshHV@cluster0.komdm2b.mongodb.net/?retryWrites=true&w=majority'
+,function(error, client){
+
+    app.listen(3000, function(){
+        console.log("Server run");
+    })
+// if DB connection finished,console.log message executed.
+})
+
 app.get("/", function(req,res){
     res.sendFile(__dirname + "/index.html");
 })
@@ -13,9 +23,8 @@ app.get("/write", function(req,res){
 
 app.post('/add',function(req,res){
     res.send("sended");
-    console.log(req.body);
+    console.log(req.body.date);
+    console.log(req.body.todo);
+    console.log(req.body.detail);
 })
 
-app.listen(3000, function(){
-    console.log("Server run");
-})
