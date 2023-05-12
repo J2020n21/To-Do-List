6880c9e7a4a4e57bf2f93180e4378aa36f2605b1
Jiui <jiui0103@naver.com>
2023-05-12 18:58:15 +0900

    Add new data to the database

--- app.js
+++ app.js
@@ -2,15 +2,23 @@ const bodyParser = require("body-parser");
 const express = require("express")
 const app = express()
 app.use(express.urlencoded({extended: true}))
-
 const MongoClient = require('mongodb').MongoClient;
+
+
+var db;
 MongoClient.connect('mongodb+srv://jiui4691:5G6jmgAHJtsJshHV@cluster0.komdm2b.mongodb.net/?retryWrites=true&w=majority'
 ,function(error, client){
+
+    if(error){return console.log(error);} //error catch
+    db = client.db('todoapp'); //connect to DB named 'todoapp'
+//save data: example
+    // db.collection('post').insertOne({Name: 'John', Age: 20 , _id:100},function(error,result){
+    //     console.log("save completed");
+    // }); 
 
     app.listen(3000, function(){
         console.log("Server run");
     })
-// if DB connection finished,console.log message executed.
 })
 
 app.get("/", function(req,res){
@@ -21,10 +29,14 @@ app.get("/write", function(req,res){
     res.sendFile(__dirname +"/write.html");
 })
 
+
 app.post('/add',function(req,res){
     res.send("sended");
-    console.log(req.body.date);
-    console.log(req.body.todo);
-    console.log(req.body.detail);
+    var todo = req.body.todo;
+    var date = req.body.date;
+    var detail = req.body.detail;
+    db.collection('post').insertOne( {Title:todo, Date:date, Detail:detail} , function(){
+        console.log("Saved");
+    });
 })
 
